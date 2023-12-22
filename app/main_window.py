40da8bb73c91b2e25933b39dffb1b602a7efe7ba
Simone Bonino <simone@binarypillow.dev>
2023-12-22 11:36:54 +0100

    Measures and rulers added (v1)

--- app/main_window.py
+++ app/main_window.py
@@ -1,10 +1,11 @@
-from PyQt6 import QtWidgets
+from PyQt6 import QtWidgets, QtGui, QtCore
 from PyQt6.QtWidgets import QColorDialog
 from matplotlib.figure import Figure
 import matplotlib.patches as patches
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import numpy as np
 import vtk
+import random
 from vtkmodules.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 import nibabel as nib
 from vtkmodules.util.numpy_support import numpy_to_vtk
@@ -92,6 +93,12 @@ class MainWindow(QtWidgets.QMainWindow):
         # Set interactor, initialize and start
         self.interactor = self.vtk_widget.GetRenderWindow().GetInteractor()
         self.interactor.Initialize()
+
+        # Create a cell picker
+        self.cell_picker = vtk.vtkCellPicker()
+        # Set the tolerance for picking a point
+        self.cell_picker.SetTolerance(0.0005)
+
         self.interactor.Start()
 
         self.default_focal = self.renderer.GetActiveCamera().GetFocalPoint()
@@ -115,6 +122,11 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.color_button.setStyleSheet(
             f"border: 0px; background-color: rgb{tuple(int(c * 255) for c in self.colors[0])}"
         )
+
+        # Create a cell picker
+        self.cell_picker = vtk.vtkCellPicker()
+        # Set the tolerance for picking a point
+        self.cell_picker.SetTolerance(0.0005)
 
         # Know the position of the camera each right-click: just for us, to better place the camera
         self.interactor.AddObserver("RightButtonPressEvent", self.get_camera_position)
@@ -157,6 +169,227 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.high_op_slider.valueChanged.connect(self.onSliderChanged)
 
         self.ui.update_button.clicked.connect(self.onVolumeUpdate)
+
+        # Create a button
+        self.ui.newruler_button.clicked.connect(self.activate_left_click_event)
+
+        self.newruler_state = False
+
+        self.selected_cells = []
+        self.selected_cell_positions = []
+        self.line_btw_cells = []
+
+        self.left_button_observer_id = 0
+
+        self.item_states = {}
+
+        volume, surface = self.calculate_volume_surface()
+
+        self.ui.volume_value.setText(f"{volume:,.2f} mm³")
+        self.ui.surface_value.setText(f"{surface:,.2f} mm²")
+
+    def add_item_with_delete_button(self, text, dist, color):
+        item = QtWidgets.QListWidgetItem(f"ruler #{text}")
+        self.ui.rulers_list.addItem(item)
+        self.ui.rulers_list.setAlternatingRowColors(True)
+        self.item_states[f"ruler #{text}"] = False
+
+        delete_button = QtWidgets.QPushButton()
+        delete_button.setIcon(
+            QtGui.QIcon(get_abs_path("resources/icons/interface/trash.svg"))
+        )  # Set the icon
+        delete_button.setStyleSheet("background-color: transparent")
+        delete_button.clicked.connect(lambda: self.delete_item(item))
+
+        hide_button = QtWidgets.QPushButton()
+        hide_button.setIcon(
+            QtGui.QIcon(get_abs_path("resources/icons/interface/eye.svg"))
+        )  # Set the icon
+        hide_button.setStyleSheet("background-color: transparent")
+        hide_button.clicked.connect(lambda: self.hide_item(item, hide_button))
+
+        color_button = QtWidgets.QPushButton()  # Create a new QPushButton
+        color_button.setStyleSheet(
+            f"background-color: rgb{tuple(int(c * 255) for c in color)}; border-radius: 4px;"
+        )
+        color_button.setMaximumSize(10, 10)  # Set the maximum width and height
+
+        label = QtWidgets.QLabel(f"{dist:.2f} mm")
+
+        widget = QtWidgets.QWidget()
+        layout = QtWidgets.QHBoxLayout(widget)
+        layout.addWidget(label)
+        layout.addWidget(color_button)
+        layout.addSpacing(5)
+        layout.addWidget(hide_button)
+        layout.addWidget(delete_button)
+        layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignRight)
+        layout.setContentsMargins(0, 0, 0, 0)
+        widget.setLayout(layout)
+
+        self.ui.rulers_list.setItemWidget(item, widget)
+
+    def delete_item(self, item):
+        row = self.ui.rulers_list.row(item)
+        self.ui.rulers_list.takeItem(row)
+        self.renderer.RemoveActor(self.selected_cells[row * 2])
+        self.renderer.RemoveActor(self.selected_cells[row * 2 + 1])
+        self.renderer.RemoveActor(self.line_btw_cells[row])
+        del self.line_btw_cells[row]
+        del self.selected_cells[row * 2 : row * 2 + 2]
+        del self.selected_cell_positions[row * 2 : row * 2 + 2]
+
+        # Render the scene
+        self.vtk_widget.GetRenderWindow().Render()
+
+        for i in range(row, self.ui.rulers_list.count()):
+            item = self.ui.rulers_list.item(i)
+            item.setText(f"ruler #{i+1}")
+            self.item_states[f"ruler #{i+1}"] = self.item_states[f"ruler #{i+2}"]
+
+        self.item_states.popitem()
+
+    def hide_item(self, item, button):
+        row = self.ui.rulers_list.row(item)
+        # Check the state of the item and set the icon accordingly
+        if self.item_states[item.text()]:
+            button.setIcon(
+                QtGui.QIcon(get_abs_path("resources/icons/interface/eye.svg"))
+            )
+            self.renderer.AddActor(self.selected_cells[row * 2])
+            self.renderer.AddActor(self.selected_cells[row * 2 + 1])
+            self.renderer.AddActor(self.line_btw_cells[row])
+        else:
+            button.setIcon(
+                QtGui.QIcon(get_abs_path("resources/icons/interface/eye-slash.svg"))
+            )
+            self.renderer.RemoveActor(self.selected_cells[row * 2])
+            self.renderer.RemoveActor(self.selected_cells[row * 2 + 1])
+            self.renderer.RemoveActor(self.line_btw_cells[row])
+
+        # Render the scene
+        self.vtk_widget.GetRenderWindow().Render()
+
+        # Toggle the state of the item
+        self.item_states[item.text()] = not self.item_states[item.text()]
+
+    def calculate_volume_surface(self):
+        selected_index = self.ui.organ_combo.currentIndex()
+        selected_surface_actor = self.segmented_surface_actors[selected_index]
+        # Convert the actor's polydata to a closed surface
+        surface_filter = vtk.vtkDataSetSurfaceFilter()
+        surface_filter.SetInputData(selected_surface_actor.GetMapper().GetInput())
+        surface_filter.Update()
+
+        # Use vtkMassProperties to compute the volume
+        mass_properties = vtk.vtkMassProperties()
+        mass_properties.SetInputData(surface_filter.GetOutput())
+        mass_properties.Update()
+
+        return mass_properties.GetVolume(), mass_properties.GetSurfaceArea()
+
+    def activate_left_click_event(self):
+        if self.newruler_state:
+            self.ui.glass_button.setDisabled(False)
+            self.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.ArrowCursor))
+            if len(self.selected_cell_positions) % 2 != 0:
+                self.renderer.RemoveActor(self.selected_cells[-1])
+                self.renderer.RemoveActor(self.selected_cells[-1])
+                del self.selected_cells[-1]
+                del self.selected_cell_positions[-1]
+            print(len(self.selected_cell_positions))
+            self.interactor.RemoveObserver(self.left_button_observer_id)
+            self.newruler_state = False
+            self.ui.newruler_button.setText("Add ruler")
+        else:
+            self.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
+            color = (random.random(), random.random(), random.random())
+            self.left_button_observer_id = self.interactor.AddObserver(
+                "LeftButtonPressEvent",
+                lambda obj, event: self.on_left_button_press(obj, event, color),
+            )
+            self.ui.glass_button.setDisabled(True)
+            self.newruler_state = True
+            self.ui.newruler_button.setText("Cancel")
+
+    def on_left_button_press(self, obj, event, color):
+        # Get the position of the mouse click event
+        click_pos = self.interactor.GetEventPosition()
+
+        # Perform the pick operation
+        self.cell_picker.Pick(click_pos[0], click_pos[1], 0, self.renderer)
+
+        # Get the picked cell
+        picked_cell = self.cell_picker.GetCellId()
+
+        if picked_cell != -1:
+            # Get the position of the picked cell
+            picked_position = self.cell_picker.GetPickPosition()
+
+            # Store the position of the picked cell
+            self.selected_cell_positions.append(picked_position)
+
+            # Create a sphere source at the picked position
+            sphere = vtk.vtkSphereSource()
+            sphere.SetCenter(picked_position)
+            sphere.SetRadius(2)  # Set the radius of the sphere
+
+            # Create a mapper
+            mapper = vtk.vtkPolyDataMapper()
+            mapper.SetInputConnection(sphere.GetOutputPort())
+
+            # Create an actor
+            actor = vtk.vtkActor()
+            actor.SetMapper(mapper)
+            actor.GetProperty().SetColor(color)
+
+            # Add the actor to the renderer
+            self.renderer.AddActor(actor)
+            self.selected_cells.append(actor)
+
+            # If two cells have been selected, calculate the distance between them and draw a dotted line
+            if len(self.selected_cell_positions) % 2 == 0:
+                distance = np.linalg.norm(
+                    np.array(self.selected_cell_positions[-2])
+                    - np.array(self.selected_cell_positions[-1])
+                )
+
+                # Create a line source
+                line_source = vtk.vtkLineSource()
+                line_source.SetPoint1(self.selected_cell_positions[-2])
+                line_source.SetPoint2(self.selected_cell_positions[-1])
+
+                # Create a mapper
+                mapper = vtk.vtkPolyDataMapper()
+                mapper.SetInputConnection(line_source.GetOutputPort())
+
+                # Create an actor
+                actor = vtk.vtkActor()
+                actor.SetMapper(mapper)
+
+                # Set the line style to dotted
+                actor.GetProperty().SetLineStipplePattern(0x00FF)
+                actor.GetProperty().SetLineStippleRepeatFactor(5)
+                actor.GetProperty().SetColor(color)
+
+                # Add the actor to the renderer
+                self.renderer.AddActor(actor)
+                self.line_btw_cells.append(actor)
+
+                self.interactor.RemoveObserver(self.left_button_observer_id)
+                self.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.ArrowCursor))
+                self.ui.glass_button.setDisabled(False)
+                self.add_item_with_delete_button(
+                    len(self.selected_cell_positions) // 2, distance, color
+                )
+                self.newruler_state = False
+                self.ui.newruler_button.setText("Add ruler")
+
+            # Render the scene
+            self.vtk_widget.GetRenderWindow().Render()
+
+        # Forward the event
+        self.interactor.InvokeEvent("LeftButtonPressEvent")
 
     def onVolumeUpdate(self):
         # Adjust opacity for a selected organ
@@ -437,6 +670,12 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.high_slider.setValue(255)
         self.ui.high_op_slider.setValue(100)
 
+        # Update volume and surface values
+        volume, surface = self.calculate_volume_surface()
+
+        self.ui.volume_value.setText(f"{volume:,.2f} mm³")
+        self.ui.surface_value.setText(f"{surface:,.2f} mm²")
+
     def onGlassButtonClicked(self):
         """
         Handles the click event of the glass button.
@@ -489,6 +728,8 @@ class MainWindow(QtWidgets.QMainWindow):
         Returns:
             None
         """
+        # Delete existing actor of the renderer
+        self.renderer.RemoveAllViewProps()
         if self.ui.volume_button.isChecked():
             # If the button is checked: launch volume rendering of the selected organ
             selected_index = self.ui.organ_combo.currentIndex()
@@ -497,13 +738,17 @@ class MainWindow(QtWidgets.QMainWindow):
                 selected_index
             ]
             self.ui.hist_group.setDisabled(False)
+            self.ui.measure_tab.setDisabled(True)
         else:
             # If the button is unchecked: launch surface rendering
             self.segmented_actors = list(self.segmented_surface_actors)
             self.ui.hist_group.setDisabled(True)
+            self.ui.measure_tab.setDisabled(False)
+            for actor in self.selected_cells:
+                self.renderer.AddActor(actor)
 
-        # Delete existing actor of the renderer
-        self.renderer.RemoveAllViewProps()
+            for actor in self.line_btw_cells:
+                self.renderer.AddActor(actor)
 
         # Add the actors to the renderer
         for actor in self.segmented_actors:
